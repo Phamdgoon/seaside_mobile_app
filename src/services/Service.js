36e2d39c74a53caf74cb99597b3b4fe83ca0729c
QuocHung3 <nongquochung0607@gmail.com>
2023-12-22 15:59:36 +0700

    s

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,85 +1,85 @@
 import axios from "axios";
 
 const handleLoginService = async (email, password) => {
-    return await axios.post("http://192.168.1.12:8080/login", {
-        email,
-        password,
-    });
+  return await axios.post("http://192.168.136.157:8088/login", {
+    email,
+    password,
+  });
 };
 const handleRegisterBuyerService = async (
+  email,
+  username,
+  password,
+  phone_number,
+  account_name
+) => {
+  return await axios.post("http://192.168.136.157:8088/register-buyer", {
     email,
     username,
     password,
     phone_number,
-    account_name
-) => {
-    return await axios.post("http://192.168.1.12:8080/register-buyer", {
-        email,
-        username,
-        password,
-        phone_number,
-        account_name,
-    });
+    account_name,
+  });
 };
 const handleRegisterSellerService = async (
+  email,
+  username,
+  password,
+  phone_number,
+  name_shop
+) => {
+  return await axios.post("http://192.168.136.157:8088/register-seller", {
     email,
     username,
     password,
     phone_number,
-    name_shop
-) => {
-    return await axios.post("http://192.168.1.12:8080/register-seller", {
-        email,
-        username,
-        password,
-        phone_number,
-        name_shop,
-    });
+    name_shop,
+  });
 };
 
 const handleGetProducts = async () => {
-    const data = await axios.get("http://192.168.1.12:8080/get-products");
-    return data;
+  const data = await axios.get("http://192.168.136.157:8088/get-products");
+  return data;
 };
 
 const handleGetCategoryService = async () => {
-    const data = await axios.get("http://192.168.1.12:8080/get-categories");
+  const data = await axios.get("http://192.168.136.157:8088/get-categories");
 
-    return data;
+  return data;
 };
 
 const handleAddNewCategory = async (name, idParent) => {
-    const res = await axios.post(
-        "http://192.168.1.12:8080/add-new-category-child",
-        {
-            nameCategoryChild: name,
-            idCategory: idParent,
-        }
-    );
+  const res = await axios.post(
+    "http://192.168.136.157:8088/add-new-category-child",
+    {
+      nameCategoryChild: name,
+      idCategory: idParent,
+    }
+  );
 
-    return res;
+  return res;
 };
 
 const handleCreateNewProduct = async (data) => {
-    const res = await axios.post(
-        "http://192.168.1.12:8080/create-new-product",
-        data
-    );
+  const res = await axios.post(
+    "http://192.168.136.157:8088/create-new-product",
+    data
+  );
 
-    return res;
+  return res;
 };
 
 const handleBuyerOrder = async (data) => {
-    return await axios.post("http://192.168.1.12:8080/buyer-order", data);
+  return await axios.post("http://192.168.136.157:8088/buyer-order", data);
 };
 
 export default handleLoginService;
 export {
-    handleRegisterBuyerService,
-    handleRegisterSellerService,
-    handleGetProducts,
-    handleGetCategoryService,
-    handleAddNewCategory,
-    handleCreateNewProduct,
-    handleBuyerOrder,
+  handleRegisterBuyerService,
+  handleRegisterSellerService,
+  handleGetProducts,
+  handleGetCategoryService,
+  handleAddNewCategory,
+  handleCreateNewProduct,
+  handleBuyerOrder,
 };
